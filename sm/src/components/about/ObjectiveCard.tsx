d150bf15502a788e5ed8552b810268c26f656676
Tanarak Chunsanit <tanarak@Tanaraks-MacBook-Pro.local>
2024-06-29 01:18:48 +0700

    fix margin

--- sm/src/components/about/ObjectiveCard.tsx
+++ sm/src/components/about/ObjectiveCard.tsx
@@ -4,7 +4,7 @@ type Props = { title: string; desc: string; iconD: string };
 
 export default function ObjectiveCard({ title, desc, iconD }: Props) {
   return (
-    <div className="sm:w-[400px] flex">
+    <div className="sm:w-[400px] flex mx-auto">
       <div className=" px-3">
         <div className="bg-secondary w-24 h-24 flex justify-center items-center rounded-xl">
           <svg
